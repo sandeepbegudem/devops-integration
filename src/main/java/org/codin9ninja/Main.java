a6c82cae4b8fa6d2c8ff233430d8b5779978d91e
TechNinja <sandeep.begudem@gmail.com>
2023-04-04 22:40:46 -0500

    modify main.java

--- src/main/java/org/codin9ninja/Main.java
+++ src/main/java/org/codin9ninja/Main.java
@@ -2,6 +2,6 @@ package org.codin9ninja;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("welcome to DevOps integration project!..");
     }
 }
